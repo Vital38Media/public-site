f61be0f6399449a963b0a067838f4c9da8e6b8b3
Keegan Donley <keegan.donley@gmail.com>
2016-10-29 23:20:56 -0700

    tweaks to wording and design

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,14 +1,14 @@
 // auto generated Javascript file, automatically added to index.html
 $(function(){
     $(".textLine").typed({
-      strings: ["<strong>38%</strong> of consumers <em>stop</em> engaging <br> if the content is unattractive",
+      strings: ["<strong>38%</strong> of consumers <em>stop</em> engaging online <br> if the content is unattractive",
         "^500Web Design | Graphic Design | Cinematography | Photography^500<br>Coming soon"],
       contentType: 'html',
       typeSpeed: 0,
       startDelay: 1500,
       showCursor: false,
       backSpeed: 0.1,
-      backDelay: 3000,
+      backDelay: 6000,
       onStringTyped: function () {
           setTimeout(function () {
               $('.secondarylogo').fadeIn(1000);
