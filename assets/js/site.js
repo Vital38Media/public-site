cc3c4c3c4ea1818917a74e5aa4b8520870546fef
Keegan Donley <keegan.donley@gmail.com>
2017-06-10 21:30:38 -0700

    Mobilize design

--- assets/js/site.js
+++ assets/js/site.js
@@ -10,24 +10,24 @@ var v38State = {
     },
     text: {
         about:  "<br>According to a report from Adobe<a class=\"inline\" href=\"http://wwwimages.adobe.com/content/dam/Adobe/en/max/2015/pdfs/state-of-content-oct.pdf\">*</a>, 38% of consumers will stop " +  
-            "engaging <br> with a product or website if the content is unattractive.<br><br>" +
-            "Vital 38 is a media production firm based in the Silicon Valley.<br>" +
+            "engaging with a product or website if the content is unattractive.<br><br>" +
+            "Vital 38 is a media production firm based in the Silicon Valley. " +
             "We work with you to create a digital media solution that is fast, " +
-            "beautiful,<br> and tailored to your every need. <br><br>With a focus on incredible, " +
-            "modern design, V38 will help small businesses,<br>companies, and individuals " +
-            "grow with a well-designed<br>and meticulously planned online presence. " +
+            "beautiful, and tailored to your every need. <br><br>With a focus on incredible, " +
+            "modern design, V38 will help small businesses, companies, and individuals " +
+            "grow with a well-designed and meticulously planned online presence. " +
             "<br>" +
             "<br>" +
             "Web design, graphic design, cinematography, and photography come " +
-            "together<br>in one seamless service, or compartmentalized services to " +
-            "meet<br>any specific need. " +
+            "together in one seamless service, or compartmentalized services to " +
+            "meet any specific need. " +
             "<br>" +
             "<br>" +
             "Don't lose the vital 38%",
         contact: "<br> We'd love to hear from you!<br><br>" +
-            "Whether you have a project in mind or just want to chat about what we do,<br>" +
+            "Whether you have a project in mind or just want to chat about what we do, " +
             "send us an email at <strong><a href=\"mailto:projects@vital38.com\">projects@vital38.com</a></strong> or call us at <strong>(650) 336-8624</strong>" +
-            "<br><br>We'd love to get to know you, and can't wait to start helping<br>" +
+            "<br><br>We'd love to get to know you, and can't wait to start helping " +
             "you achieve your dream, whatever that may be."
     }
 
