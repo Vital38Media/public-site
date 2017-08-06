b220acf6308a15527326c60e8f4447b54bb181a3
Mbecnel <mauricebecnel400>
2017-08-06 15:52:07 -0700

    Created a function that makes new typed objects for the scroll function.

--- assets/js/site.js
+++ assets/js/site.js
@@ -9,7 +9,7 @@ var v38State = {
         about: 625
     },
     text: {
-        about:  "<br>According to a report from Adobe<a class=\"inline\" href=\"http://wwwimages.adobe.com/content/dam/Adobe/en/max/2015/pdfs/state-of-content-oct.pdf\">*</a>, 38% of consumers will stop " +  
+        about:  "<br>According to a report from Adobe<a class=\"inline\" href=\"http://wwwimages.adobe.com/content/dam/Adobe/en/max/2015/pdfs/state-of-content-oct.pdf\">*</a>, 38% of consumers will stop " +
             "engaging with a product or website if the content is unattractive.<br><br>" +
             "Vital 38 is a media production firm based in the Silicon Valley. " +
             "We work with you to create a digital media solution that is fast, " +
@@ -32,51 +32,38 @@ var v38State = {
     }
 };
 
+function trace (title) {
+  v38State.displays[title] = true;
+  var callback = null;
+  if (title === 'services') {
+    callback = function() {
+        $('.servicesContent').slideDown();
+    };
+  } else {
+    callback = function() {
+      Typed.new('.' + title + 'Text', {
+          strings: [v38State.text[title]],
+          showCursor: false,
+          typeSpeed: -50000,
+        });
+      };
+    }
+  Typed.new('.' + title + 'Header', {
+     strings: [title[0].toUpperCase() + title.slice(1)],
+      showCursor: true,
+      cursorChar: "|",
+      typeSpeed: 1,
+      callback: callback
+        });
+}
+
 $(window).scroll(function(){
     if ($(this).scrollTop() > $('.contact').position().top - (window.innerHeight/3*2) && !v38State.displays.contact) {
-        v38State.displays.contact = true;
-        Typed.new('.contactHeader', {
-           strings: ["Contact"],
-            showCursor: true,
-            cursorChar: "|",
-            typeSpeed: 1,
-            callback: function() {
-                Typed.new('.contactText', {
-                    strings: [v38State.text.contact],
-                    showCursor: false,
-                    typeSpeed: -50000,
-                    
-	            });
-            },
-	    });
+      trace('contact');
     } else if ($(this).scrollTop() > $('.services').position().top - (window.innerHeight/3*2) && !v38State.displays.services) {
-        v38State.displays.services = true;
-        Typed.new('.servicesHeader', {
-           strings: ["Services"],
-            showCursor: true,
-            cursorChar: "|",
-            typeSpeed: 1,
-            callback: function() {
-                $('.servicesContent').slideDown();
-            },
-	    });
-
+      trace('services');
     }  else if ($(this).scrollTop() > $('.about').position().top - (window.innerHeight/3*2) && !v38State.displays.about) {
-        v38State.displays.about = true;
-        Typed.new('.aboutHeader', {
-           strings: ["About"],
-            showCursor: true,
-            cursorChar: "|",
-            typeSpeed: 1,
-            callback: function() {
-                Typed.new('.aboutText', {
-                    strings: [v38State.text.about],
-                    showCursor: false,
-                    typeSpeed: -50000,
-                    
-	            });
-            },
-	    });
+      trace('about');
     }
 });
 
