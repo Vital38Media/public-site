972fa4b1160c44cd5e8b950627920ae490066189
Keegan Donley <keegan.donley@gmail.com>
2017-08-29 12:58:50 -0700

    clean up intro sequence

--- assets/js/init.js
+++ assets/js/init.js
@@ -8,36 +8,44 @@ $(document).ready(function() {
     }
 });
 
-document.addEventListener('DOMContentLoaded', function(){
-    setTimeout(function() {
-        var fadeDelay = 2000;
-        $('#loadingCover').fadeOut(fadeDelay);
-        $('#pulse').fadeOut(fadeDelay/2);
-        setTimeout(function() {
-            for (var property in v38State.displays) {
-                v38State.displays[property] = false;
-            }
-            $('.headerLogo').fadeIn();
-            Typed.new('.topTag', {
-                strings: ['&lt;p class="intro">'],
+function animateIntro() {
+    for (var property in v38State.displays) {
+        v38State.displays[property] = false;
+    }
+    $('.headerLogo').fadeIn();
+    Typed.new('.topTag', {
+        strings: ['&lt;p class="intro">'],
+        showCursor: false,
+        typeSpeed: 1,
+        callback: function() {
+            Typed.new('.bodyTag', {
+                stringsElement: document.getElementById("typed-strings"),
                 showCursor: false,
                 typeSpeed: 1,
                 callback: function() {
-                    Typed.new('.bodyTag', {
-                        stringsElement: document.getElementById("typed-strings"),
+                    Typed.new('.bottomTag', {
+                        strings: ['&lt;/p>'],
                         showCursor: false,
                         typeSpeed: 1,
-                        callback: function() {
-                            Typed.new('.bottomTag', {
-                                strings: ['&lt;/p>'],
-                                showCursor: false,
-                                typeSpeed: 1,
-                            });
-                        }
                     });
                 }
             });
-        }, fadeDelay - 500);
+        }
+    });
+}
+
+function processCover() {
+    var fadeDelay = 2000;
+    $('#loadingCover').fadeOut(fadeDelay);
+    $('#pulse').fadeOut(fadeDelay/2);
+    setTimeout(function() {
+        animateIntro();
+    }, fadeDelay - 500);
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    setTimeout(function() {
+        processCover();
     }, 3000);
 
    
